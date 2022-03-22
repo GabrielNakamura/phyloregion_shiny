caca762cf8988cc0dceb9dc1e3bf438c58b3f428
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-03-22 11:39:26 -0500

    updating zoom function for phylogenetic tree

--- www/function_zoom_phylo.R
+++ www/function_zoom_phylo.R
@@ -1,28 +1,9 @@
 ## zoom in and out phylogenies 
 
-library(phyloregion)
-library(magrittr)
-library(plotly)
-library(ggtree)
-library(ggplot2)
-data("africa")
-tree <- africa$phylo
-tree$tip.label
-
-
-
-
-phylo <- africa$phylo
-tree <- phylo
 plot_interact <- 
   function(tree,  height, width, type = "circular", tip.label = FALSE){
     plot_tree <- ggtree::ggtree(tree, layout = type)
-    plot_tree <- ggtree::ggtree(tree, layout = "rectangular")
+    plot_tree <- ggtree::ggtree(tree, layout = "rectangular") +
+      theme_tree2()
     plot_tree %>% plotly::ggplotly(height = height, width = width)
 } 
-plot_interact(tree = phylo, 
-              type = "circular",
-              tip.label = FALSE)
-plot_tree <- ggtree::ggtree(tree, layout = "circular")
-plot_tree %>% plotly::ggplotly()
-help(package = "phyloregion")
